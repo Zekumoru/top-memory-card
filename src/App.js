11f8b2f5ecb8084a07e87f02afdb34e9e1a1224a
Zekumoru Dragonhart <dev@zekumoru.com>
2023-01-30 15:46:42 +0100

    Add score div

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import './styles/App.scss';
 import Cards from './components/Cards';
 import Header from './components/Header';
 import usePokemon from './hooks/usePokemon';
+import { useState } from 'react';
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
   const { pokemons } = usePokemon();
 
   return (
     <div className="App">
       <Header />
+      <div className="score">
+        <h2>Score: {score}</h2>
+        <p>Best score: {bestScore}</p>
+      </div>
       <Cards items={pokemons} />
     </div>
   );
