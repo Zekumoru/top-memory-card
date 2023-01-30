2d3596db21390a7e55463bb5d29b8a16be055e79
Zekumoru Dragonhart <dev@zekumoru.com>
2023-01-30 14:38:31 +0100

    Create Header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="Header">
+      <h1>Pokémon Memory</h1>
+      <p>Click on one of the Pokémon but do not pick it twice!</p>
+    </div>
+  );
+}
+
+export default Header;
