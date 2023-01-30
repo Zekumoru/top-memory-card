0a1bc5d2b73038a7da7825a276919ec4e228c341
Zekumoru Dragonhart <dev@zekumoru.com>
2023-01-30 16:36:04 +0100

    Rename array param to initialArray

--- src/hooks/useArrayShuffler.js
+++ src/hooks/useArrayShuffler.js
@@ -1,21 +1,21 @@
 import { useCallback, useState } from 'react';
 
-function useArrayShuffler(array) {
-  const [arr, setArr] = useState(array);
+function useArrayShuffler(initialArray) {
+  const [array, setArray] = useState(initialArray);
 
   const shuffle = useCallback(() => {
-    setArr((arr) => {
+    setArray((array) => {
       const shuffled = [];
-      for (let i = arr.length - 1; i >= 0; i--) {
+      for (let i = array.length - 1; i >= 0; i--) {
         const randomIndex = Math.floor(Math.random() * (i + 1));
-        shuffled.push(arr[randomIndex]);
-        [arr[i], arr[randomIndex]] = [arr[randomIndex], arr[i]];
+        shuffled.push(array[randomIndex]);
+        [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
       }
       return shuffled;
     });
   }, []);
 
-  return [arr, shuffle];
+  return [array, shuffle];
 }
 
 export default useArrayShuffler;
